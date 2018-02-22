68dc978ba08ddac9678dfd50893f1dc9945b516a
chovanec35 <jozefcchovanec@gmail.com[merge]>
2018-02-22 13:47:22 +0100

    add function for move

--- mavenproject3/src/main/java/MainFrame.java
+++ mavenproject3/src/main/java/MainFrame.java
@@ -1,10 +1,15 @@
 
 import java.awt.Color;
+import java.awt.Label;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javafx.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MainFrame {  
+public class MainFrame {
 
     public void create() {
         JFrame window = new JFrame("Aplication");
@@ -13,11 +18,19 @@ public class MainFrame {
         window.setResizable(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JLabel l = new JLabel();
+        l.setSize(350,100);
+        l.setBounds(200, 200, 200,200);
+        l.setText("Submit Button clicked.");
+//        l.setVisible(true);
+        window.add(l);
+
         JPanel panel = new JPanel();
-        panel.setBackground(Color.red);
+//        panel.setBackground(Color.red);
         panel.setLayout(null);
         panel.setBounds(0, 0, 100, 101);
         window.add(panel);
+        panel.add(l);
 
         JButton[] buttons = new JButton[4];
         String[] names = {"^", "v", "<", ">"};
@@ -31,5 +44,13 @@ public class MainFrame {
         buttons[1].setBounds(50, 100, 50, 50);
         buttons[2].setBounds(0, 50, 50, 50);
         buttons[3].setBounds(100, 50, 50, 50);
+
+        buttons[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                System.out.println("naaajs");
+                
+            }
+        });
     }
 }
