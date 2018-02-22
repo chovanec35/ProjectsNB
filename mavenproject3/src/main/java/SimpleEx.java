b0a65cbe1ba045b83af9863e69658543f258b3c1
chovanec35 <jozefcchovanec@gmail.com>
2018-02-22 10:02:27 +0100

    test01

--- mavenproject3/src/main/java/SimpleEx.java
+++ mavenproject3/src/main/java/SimpleEx.java
@@ -7,30 +7,7 @@ import javax.swing.JPanel;
 public class SimpleEx {
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("");
-        window.setVisible(true);
-        window.setSize(500, 500);
-        window.setResizable(true);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.red);
-        panel.setLayout(null);
-        panel.setBounds(0, 0, 100, 101);
-        window.add(panel);
-
-        JButton[] buttons = new JButton[4];
-        String[] names = {"^", "v", "<", ">"};
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton(names[i]);
-            System.out.println(buttons[i].getText());
-            panel.add(buttons[i]);
-        }
-
-        buttons[0].setBounds(50, 0, 50, 50);
-        buttons[1].setBounds(50, 100, 50, 50);
-        buttons[2].setBounds(0, 50, 50, 50);
-        buttons[3].setBounds(100, 50, 50, 50);
+        MainFram frame = new MainFrame();
 
     }
 
